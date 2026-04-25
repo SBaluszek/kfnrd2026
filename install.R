0c92886e091f0fe5682cd7b508a81d959f819c16
Szymon <50032467+SBaluszek@users.noreply.github.com>
2026-04-25 09:38:43 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,2 +1,2 @@
 install.packages(c("tidyverse", "stringdist", "ggplot2", "gtools", "purrr"))
-BiocManager::install(c("Biostrings", "QuasR", "Rhisat2"))
+BiocManager::install(c("Biostrings", "QuasR", "Rhisat2", "maftools", "PoisonAlien/TCGAmutations", "BSgenome.Hsapiens.UCSC.hg19"))
