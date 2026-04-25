bf181271b6f072dcaa307beccd7f84e03a49ed8a
Szymon <50032467+SBaluszek@users.noreply.github.com>
2026-04-25 10:41:11 +0200

    Add files via upload

--- install.R
+++ install.R
@@ -1,2 +1,4 @@
-install.packages(c("tidyverse", "stringdist", "ggplot2", "gtools", "purrr"))
-BiocManager::install(c("Biostrings", "QuasR", "Rhisat2", "maftools", "PoisonAlien/TCGAmutations", "BSgenome.Hsapiens.UCSC.hg19"))
+install.packages(c("tidyverse", "stringdist", "ggplot2", "gtools", "purrr", "mclust", "R.utils", "NMF", "patchwork", "uwot", "arrow", "hexbin", "lightAUC"))
+#BiocManager::install(c("Biostrings", "QuasR", "Rhisat2"))
+BiocManager::install(c("Biostrings", "maftools", "PoisonAlien/TCGAmutations", "BSgenome.Hsapiens.UCSC.hg19"))
+#BiocManager::install(c("msigdbr", "DESeq2", "Rhisat2", "viridis", "fgsea", "vsn"))
