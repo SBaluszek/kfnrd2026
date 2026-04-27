a716f05cb9788a03d568073115787181ac883f66
Szymon <50032467+SBaluszek@users.noreply.github.com>
2026-04-27 09:53:39 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
-install.packages(c("tidyverse", "stringdist", "ggplot2", "gtools", "purrr", "mclust", "R.utils", "NMF", "patchwork", "uwot", "arrow", "hexbin", "lightAUC"))
+install.packages(c("tidyverse", "stringdist", "ggplot2", "gtools", "purrr", "mclust", "R.utils", "NMF", "patchwork", "uwot", "arrow", "hexbin", "lightAUC", "pheatmap"))
 #BiocManager::install(c("Biostrings", "QuasR", "Rhisat2"))
 #BiocManager::install(c("Biostrings", "maftools", "PoisonAlien/TCGAmutations", "BSgenome.Hsapiens.UCSC.hg19"))
 BiocManager::install(c("msigdbr", "DESeq2", "viridis", "fgsea", "vsn"))
